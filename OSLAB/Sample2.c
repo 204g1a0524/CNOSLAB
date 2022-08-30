afc43db3ed75fd98c8ff2da7aa8593e4e01440e2
204g1a0524 <112081413+204g1a0524@users.noreply.github.com>
2022-08-30 12:02:23 +0530

    K Bramha teja completed Sample2.c

---
+++ OSLAB/Sample2.c
@@ -0,0 +1,17 @@
+/* Name Of the Candidate:K Bramha teja
+   Roll No: 204G1A0524
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 30-08-2022
+   Date of Execution: 30-08-2022
+*/
+#include <stdio.h>
+int main()
+{
+    int x = 20;
+    int y = 22;
+    if (x<y)
+    {
+        printf("Variable x is less than y");
+    }
+    return 0;
+}
