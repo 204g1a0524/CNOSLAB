80860a9ebc7a168204d1ab8a84bf6754c331cb08
204g1a0524 <112081413+204g1a0524@users.noreply.github.com>
2022-08-30 11:48:54 +0530

    Update Sample.c

--- OSLAB/Sample.c
+++ OSLAB/Sample.c
@@ -4,22 +4,8 @@
    Date of Creation: 30-08-2022
    Date of Execution: 30-08-2022
 */
-
-   #include <stdio.h>  
-void swap(int , int); 
-int main()  
-{  
-    int a = 10;  
-    int b = 20;   
-    printf("Before swapping the values in main a = %d, b = %d\n",a,b);  
-    swap(a,b);  
-    printf("After swapping values in main a = %d, b = %d\n",a,b);    
-}  
-void swap (int a, int b)  
-{  
-    int temp;   
-    temp = a;  
-    a=b;  
-    b=temp;  
-    printf("After swapping values in function a = %d, b = %d\n",a,b); 
-}
+// Source Code
+    #include <stdio.h>
+    int main(){
+	printf("Hello World\n");
+  
