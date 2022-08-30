4d1dcc78c5e00509b4ec32616e555bd29f03bc8f
204g1a0524 <112081413+204g1a0524@users.noreply.github.com>
2022-08-30 11:52:35 +0530

    Update Sample.c

--- OSLAB/Sample.c
+++ OSLAB/Sample.c
@@ -4,8 +4,21 @@
    Date of Creation: 30-08-2022
    Date of Execution: 30-08-2022
 */
-// Source Code
-    #include <stdio.h>
-    int main(){
-	printf("Hello World\n");
-  
+    #include <stdio.h>  
+    void swap(int , int); //prototype of the function   
+    int main()  
+    {  
+        int a = 10;  
+        int b = 20;   
+        printf("Before swapping the values in main a = %d, b = %d\n",a,b); // printing the value of a and b in main  
+        swap(a,b);  
+        printf("After swapping values in main a = %d, b = %d\n",a,b); // The value of actual parameters do not change by changing the formal parameters in call by value, a = 10, b = 20  
+    }  
+    void swap (int a, int b)  
+    {  
+        int temp;   
+        temp = a;  
+        a=b;  
+        b=temp;  
+        printf("After swapping values in function a = %d, b = %d\n",a,b); // Formal parameters, a = 20, b = 10   
+    }  
