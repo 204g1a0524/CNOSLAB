472cf13efd4acccc3dd6b226286b6644318c8fa2
204g1a0524 <112081413+204g1a0524@users.noreply.github.com>
2022-11-15 12:51:14 +0530

    created DLP.c

---
+++ exp1/DLP.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate:K.BRAMHA TEJA
+   Roll No: 204G1A0524 
+   Title of the Experiment: BANKERS DLA Algorithm
+   Date of Creation: 15-11-2022
+   Date of Execution: 15-11-2022
+*/
