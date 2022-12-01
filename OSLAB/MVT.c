940c3616ed0a1e4f55f59cfc0df56e92940564a3
204G1A0524 <204G1A0524@srit.ac.in>
2022-12-01 15:38:28 +0530

    succesfully executed

--- OSLAB/MVT.c
+++ OSLAB/MVT.c
@@ -4,12 +4,11 @@
    Date of Execution: 22-11-2022
 */
 #include<stdio.h>
-#include<conio.h>
-main()
+
+void main()
 {
-int ms, bs, nob, ef,n,
-mp[10],tif=0; int i,p=0;
-clrscr();
+int ms, bs, nob, ef,n, mp[10],tif=0; 
+int i,p=0;
 printf("Enter the total memory available (in Bytes) -- ");
 scanf("%d",&ms);
 printf("Enter the block size (in Bytes) -- ");
@@ -24,8 +23,7 @@ printf("Enter memory required for process %d (in Bytes)-- ",i+1);
 scanf("%d",&mp[i]);
 }
 printf("\nNo. of Blocks available in memory--%d",nob);
-printf("\n\nPROCESS\tMEMORYREQUIRED\tALLOCATED\tINTERNAL
-FRAGMENTATION");
+printf("\n\nPROCESS\tMEMORYREQUIRED\tALLOCATED\tINTERNAL FRAGMENTATION");
 for(i=0;i<n && p<nob;i++)
 {
 printf("\n %d\t\t%d",i+1,mp[i]);
@@ -43,4 +41,5 @@ if(i<n)
 printf("\nMemory is Full, Remaining Processes cannot be accomodated");
 printf("\n\nTotal Internal Fragmentation is %d",tif);
 printf("\nTotal External Fragmentation is %d",ef);
-getch();
+}
+
